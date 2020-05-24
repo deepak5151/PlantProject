e111ba50f1b7abad79b828ae14ae74a7fbf4af3d
Deepak Kapoor <nk93555@gmail.com>
2020-05-24 20:44:49 +0530

    Updates

---
+++ precache-manifest.f654b126d1c12578882d60004e44cf3b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "841dacd1dae48004c36a3e4e406a0b46",
+    "url": "/PlantProject/index.html"
+  },
+  {
+    "revision": "b1370b6cb006954aeb6b",
+    "url": "/PlantProject/static/css/2.d35fae39.chunk.css"
+  },
+  {
+    "revision": "c73ca3a00da5d1f996de",
+    "url": "/PlantProject/static/css/main.c0efa070.chunk.css"
+  },
+  {
+    "revision": "b1370b6cb006954aeb6b",
+    "url": "/PlantProject/static/js/2.e2f02458.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/PlantProject/static/js/2.e2f02458.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c73ca3a00da5d1f996de",
+    "url": "/PlantProject/static/js/main.52186503.chunk.js"
+  },
+  {
+    "revision": "d45c01fdee08a4e523f4",
+    "url": "/PlantProject/static/js/runtime-main.32d73fc0.js"
+  },
+  {
+    "revision": "55d5567d5080cf9a8f294bad872ffc08",
+    "url": "/PlantProject/static/media/blur-background-1187974.55d5567d.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/PlantProject/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/PlantProject/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/PlantProject/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/PlantProject/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/PlantProject/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
